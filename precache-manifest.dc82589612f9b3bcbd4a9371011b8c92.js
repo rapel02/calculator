7670ff670197d806245a333bdbdff467bd607104
rafaelherman <rafaelyosef1997@gmail.com>
2019-06-16 01:30:31 +0700

    Updates

---
+++ precache-manifest.dc82589612f9b3bcbd4a9371011b8c92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24fc2b3f65878c72ecfeaf1d6f332f7a",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "4bf29c637cb5efedee33",
+    "url": "/calculator/static/css/main.05ca56f5.chunk.css"
+  },
+  {
+    "revision": "ec17dd9d751765d291bb",
+    "url": "/calculator/static/js/2.609907aa.chunk.js"
+  },
+  {
+    "revision": "4bf29c637cb5efedee33",
+    "url": "/calculator/static/js/main.e7ee9ebd.chunk.js"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  }
+]);
